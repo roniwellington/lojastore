5567ef22664f0662e679adf282f5e581fc8f8ea1
Roni Wellington <roniwellington@gmail.com>
2022-12-14 19:04:08 -0300

    pagina home completa

--- script.js
+++ script.js
@@ -47,4 +47,16 @@ const swiper = new Swiper('.swiper', {
     pagination: {
       el: '.swiper-pagination',
     },
+
   });
+
+  //show search
+  const searchButton = document.querySelector('.t-search'),
+     tClose = document.querySelector('.search-close')
+     showClass = document.querySelector('.site')
+searchButton.addEventListener('click', function(){
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function(){
+    showClass.classList.remove('showsearch')
+})
